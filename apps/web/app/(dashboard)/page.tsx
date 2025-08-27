e72894bbab320c1d9bc46772d484c72fac55ae2e
Muhammad Hamza <mhamzaawanmp1@gmail.com>
2025-08-27 10:19:24 -0700

    05: Error Tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <OrganizationSwitcher hidePersonal />
         <Button onClick={() => addUser()}>Add User</Button>
-        <h1 className="text-2xl font-bold">Hello Apps web</h1>
-        {JSON.stringify(users)}
       </div>
     </div>
   );
---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3c1525ac5bfab93616a48d904d763338@o4509320607891456.ingest.de.sentry.io/4509916805529680",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
